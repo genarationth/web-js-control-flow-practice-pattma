825cad56da6c27eb4d3ea9d046c24483c83c4de0
pattma <57069135+pattma@users.noreply.github.com>
2023-03-13 14:44:59 +0700

    update Ternary Operator

--- ternary-operator.js
+++ ternary-operator.js
@@ -1,2 +1,2 @@
-const password = 'pas$word';
+const password = 'password';
 password.length >= 12 ? console.log('password is very strong so WELCOME!!!') : password.length >= 8 ? console.log('WELCOME!!!') : console.log('SORRY!!! the password is too short');
